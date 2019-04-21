f10867311f269e7eb38dcb2ac01591ea46717ac1
Yogeesh R <ryogeesh@amazon.com>
2019-04-21 18:05:23 +0530

    Changes GM Script of WhatsApp and added Sample Image

--- WhatsApp/whatsappEnhancer.js
+++ WhatsApp/whatsappEnhancer.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         WhatsApp Tamper
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       yogeesh.srkvs@gmail.com
 // @match        https://web.whatsapp.com/*
 // @grant        none
 // ==/UserScript==
@@ -23,19 +23,46 @@
 
         var allSpans = document.body.getElementsByTagName("span");
 
+        // request permission on page load
+                  document.addEventListener('DOMContentLoaded', function () {
+                    if (!Notification) {
+                      alert("bad");
+                      return;
+                    }
+
+                    if (Notification.permission !== "granted")
+                      Notification.requestPermission();
+                  });
+
         setInterval(function() {
           for (var i=0; i<allSpans.length; i++) {
 
-        if (allSpans[i].dir=="auto") {
-        		var name = allSpans[i].title;
-        	}
+            if (allSpans[i].dir=="auto") {
+                var name = allSpans[i].title;
+            }
 
-        	if (allSpans[i].title=="online") {
-        		console.log(new Date()+" , "+name+ " , " + allSpans[i].title);
-        	}
+            if (allSpans[i].title=="online") {
+                console.log(new Date()+" , "+name+ " , " + allSpans[i].title);
+
+
+
+                    if (Notification.permission !== "granted")
+                      Notification.requestPermission();
+                    else {
+                      var notification = new Notification(name + " is online", {
+                        body: new Date(),
+                      });
+
+                      notification.onclick = function () {
+                        window.open("https://web.whatsapp.com/");
+                      };
+
+                    }
+
+            }
+
         }
-        }, 5000); // 5000 milliseconds
-
+      }, 5000); // 5000 milliseconds
     }
 
 })();
